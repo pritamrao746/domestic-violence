1f07ef0e0f078777b951ed96f5131c37f74b0be3
bhargavi1poyekar <bhargavi1poyekar@gmail.com>
2021-04-03 19:30:26 +0530

    Ui for emergency button

--- lib/home-views/emergency.dart
+++ lib/home-views/emergency.dart
@@ -9,8 +9,44 @@ class Emergency extends StatefulWidget {
 }
 
 class _EmergencyState extends State<Emergency> {
+  final Color primaryColor=Color(0xff18203d);
+  final Color secondaryColor = Color(0xff232c51);
+  final Color logoGreen=Color(0xff25bcbb);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          title:Text('Emergency'),
+          backgroundColor: primaryColor,
+          actions:[
+            IconButton(icon: Icon(Icons.g_translate, color:Colors.white) ,onPressed: null)
+          ]
+      ),
+      body: Center(
+        child: Container(
+            child:ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  primary: Colors.red,
+                  elevation: 50,
+              ),
+              child: Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+
+                ),
+                child: Text(
+                  'Press',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+              onPressed: () {},
+            )
+        ),
+      ),
+    );
   }
 }
