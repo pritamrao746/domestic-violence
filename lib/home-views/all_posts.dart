2ae0572363886cdaaca458e6da8cb1a022876a0c
mahipal purohit <mahipalkeizer@gmail.com>
2021-04-07 05:12:16 +0530

    all posts added

--- lib/home-views/all_posts.dart
+++ lib/home-views/all_posts.dart
@@ -3,6 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:domestic_violence/home-views/comments.dart';
+import 'package:http/http.dart' as http;
+import 'package:encrypt/encrypt.dart' as enc;
+import 'dart:typed_data' show Uint8List;
+
+import '../MyEncrpytClass.dart';
 
 class AllPosts extends StatefulWidget {
   @override
@@ -11,7 +16,7 @@ class AllPosts extends StatefulWidget {
 
 class _AllPostsState extends State<AllPosts> {
 
-  Widget _buildPost(int index, name, time, link,caption){
+  Widget _buildPost(int index, name, time, Uint8List decry,caption){
 
     return Padding(
       padding:EdgeInsets.symmetric(horizontal: 10.0,vertical:5.0),
@@ -83,7 +88,7 @@ class _AllPostsState extends State<AllPosts> {
                                     ),
                                   ],
                                   image:DecorationImage(
-                                    image:AssetImage(link),
+                                    image:MemoryImage(decry, scale: 0.5),
                                     fit:BoxFit.fitWidth,
                                   )
                               )
@@ -168,8 +173,8 @@ class _AllPostsState extends State<AllPosts> {
     FirebaseAuth auth = FirebaseAuth.instance;
     String uid = auth.currentUser.uid;
 
-
-    List<Widget> m=[_buildPost(0,'keizer','12:04 15th March','assets/.jpg','caption'),
+    var a=[255, 216, 255, 225, 2, 168, 69, 120, 105, 102, 0, 0, 77, 77, 0, 42, 0, 0, 0, 8, 0, 8, 1, 0, 0, 3, 0, 0, 0, 1, 7, 152, 0, 0, 1, 16, 0, 2, 0, 0, 0, 13, 0, 0, 0, 110, 1, 1, 0, 3, 0, 0, 0, 1, 7, 152, 0, 0, 1, 15, 0, 2, 0, 0, 0, 7, 0, 0, 0, 123, 135, 105, 0, 4, 0, 0, 0, 1, 0, 0, 0, 150, 1, 18, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 1, 50, 0, 2, 0, 0, 0, 20, 0, 0, 0, 130, 136, 37, 0, 4, 0, 0, 0, 1, 0, 0, 1, 97, 0, 0, 0, 0, 82, 101, 100, 109, 105, 32, 78, 111, 116, 101, 32, 53, 0, 88, 105, 97, 111, 109, 105, 0, 50, 48, 50, 49, 58, 48, 51, 58, 51, 48, 32, 49, 54, 58, 50, 56, 58, 50, 57, 0, 0, 11, 146, 145, 0, 2, 0, 0, 0, 7, 0, 0, 1, 32, 164, 3, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 144, 4, 0, 2, 0, 0, 0, 20, 0, 0, 1, 39, 146, 10, 0, 5, 0, 0, 0, 1, 0, 0, 1, 59, 130, 154, 0, 5, 0, 0, 0, 1, 0, 0, 1, 67, 136, 39, 0, 3, 0, 0, 0, 1, 3, 32, 0, 0, 146, 9, 0, 3, 0, 0, 0, 1, 0, 16, 0, 0, 146, 144, 0, 2, 0, 0, 0, 7, 0, 0, 1, 75, 146, 146, 0, 2, 0, 0, 0, 7, 0, 0, 1, 82, 146, 8, 0, 4, 0, 0, 0, 1, 0, 0, 0, 0, 130, 157, 0, 5, 0, 0, 0, 1, 0];
+    List<Widget> m=[_buildPost(0,'keizer','12:04 15th March',Uint8List.fromList(a),'caption'),
           ];
 
     FirebaseFirestore.
@@ -177,17 +182,28 @@ class _AllPostsState extends State<AllPosts> {
     collection("image").
     get().
     then((value) {
-      value.docs.forEach((result) {
+      value.docs.forEach((result) async {
         print("-----DEBUG-----");
         String username=result.data()['uid'];
         String captioin=result.data()['caption'];
         String time= result.data()['time'];
         String url=result.data()['url'];
+
         print('$username, $captioin, $time, $url');
-        m.add(_buildPost(0,username,time, 'assets/post1.jpg',captioin) );
+        var resp = await http.get(url);
+        enc.Encrypted encc = enc.Encrypted(resp.bodyBytes);
+        print("RESP = ${resp.bodyBytes}");
+
+        List<int> decryptedData =
+        MyEncrypt.myEncrypter.decryptBytes(encc, iv: MyEncrypt.myIv);
+        Uint8List d = Uint8List.fromList(decryptedData);
+        var decr = d;
+        print("RESP = ${decryptedData}");
+        m.add(_buildPost(0,username,time, decr,captioin) );
       });
     });
 
+    print(m);
     ListView l=ListView(
       physics:AlwaysScrollableScrollPhysics(),
       children: m,
--- lib/home-views/video_list.dart
+++ lib/home-views/video_list.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:domestic_violence/home-views/display_video.dart';
@@ -13,8 +15,35 @@ class _VideoListState extends State<VideoList> {
   final Color secondaryColor = Color(0xff232c51);
   final Color logoGreen=Color(0xff25bcbb);
   final List<dynamic> videos = video_url;
+
+
+
+
   @override
   Widget build(BuildContext context) {
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+//    String uid = auth.currentUser.uid;
+    String uid='HVaUYC72SvhbsFOpOmC9YmLPk9B2';
+
+    var url_list=[];
+    FirebaseFirestore.
+    instance.
+    collection("video").
+    where("uid", isEqualTo: uid).
+    get().
+    then((value) {
+      value.docs.forEach((result) {
+        var avd = new Map();
+        avd['title'] = result.data()['time'];
+        avd['url'] = result.data()['url'];
+        print(avd);
+        url_list.add(avd);
+      });
+    });
+    print(url_list);
+
+
     return Scaffold(
       body: ListView.builder(
           itemCount: videos.length,
@@ -29,6 +58,7 @@ class _VideoListState extends State<VideoList> {
                   IconButton(
                     icon: Icon(Icons.play_arrow),
                     onPressed: () {
+
                       Navigator.push(context,
                           MaterialPageRoute(builder: (_) => VideoDemo(url:videos[index]["url"])));
                     },
